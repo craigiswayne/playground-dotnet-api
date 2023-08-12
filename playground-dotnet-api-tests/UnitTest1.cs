636c654da2a75088e1914308db9f08ffc501827c
Craig Wayne <craigiswayne@gmail.com>
2023-08-13 00:12:01 +0100

    Adding test project using ui

---
+++ playground-dotnet-api-tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace playground_dotnet_api_tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
