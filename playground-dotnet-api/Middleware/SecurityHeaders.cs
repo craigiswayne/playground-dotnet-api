5adaff62395c403cd6652a6fed565aff32676e4b
Craig Wayne <craigiswayne@gmail.com>
2023-08-13 22:47:39 +0100

    Reordered security headers

--- playground-dotnet-api/Middleware/SecurityHeaders.cs
+++ playground-dotnet-api/Middleware/SecurityHeaders.cs
@@ -16,39 +16,34 @@ public class SecurityHeadersMiddleware
             context.Response.Headers.Add("Content-Security-Policy", "default-src 'self';");
         }
 
-
-        if (!context.Response.Headers.ContainsKey("X-Frame-Options"))
+        if (!context.Response.Headers.ContainsKey("Permissions-Policy"))
         {
-            context.Response.Headers.Add("X-Frame-Options", "DENY");
+            context.Response.Headers.Add("Permissions-Policy", "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()");
         }
 
-
-        if (!context.Response.Headers.ContainsKey("X-Xss-Protection"))
+        if (!context.Response.Headers.ContainsKey("Referrer-Policy"))
         {
-            context.Response.Headers.Add("X-Xss-Protection", "1; mode=block");
+            context.Response.Headers.Add("Referrer-Policy", "same-origin");
         }
-
 
         if (!context.Response.Headers.ContainsKey("X-Content-Type-Options"))
         {
             context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
         }
 
-
-        if (!context.Response.Headers.ContainsKey("Referrer-Policy"))
+        if (!context.Response.Headers.ContainsKey("X-Frame-Options"))
         {
-            context.Response.Headers.Add("Referrer-Policy", "no-referrer");
+            context.Response.Headers.Add("X-Frame-Options", "DENY");
         }
-
 
         if (!context.Response.Headers.ContainsKey("X-Permitted-Cross-Domain-Policies"))
         {
             context.Response.Headers.Add("X-Permitted-Cross-Domain-Policies", "none");
         }
 
-        if (!context.Response.Headers.ContainsKey("Permissions-Policy"))
+        if (!context.Response.Headers.ContainsKey("X-Xss-Protection"))
         {
-            context.Response.Headers.Add("Permissions-Policy", "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()");
+            context.Response.Headers.Add("X-Xss-Protection", "1; mode=block");
         }
 
         await _next(context);
